b7308eab038a0789e4d87f0ec844824a0020a142
ANDREW LAM <23492362+andrewclam1991@users.noreply.github.com>
2021-05-02 13:22:04 -0700

    Adds react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,43 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Quote } from './features/quote/Quote';
 import { Markdown } from './features/markdown/Markdown';
 
-function App() {
+export default function App() {
   return (
-    <>
-      <Quote />
-      <Markdown />
-    </>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/random-quote-generator">Random Quote Generator</Link>
+            </li>
+            <li>
+              <Link to="/markdown-previewer">Markdown Previewer</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <Switch>
+        <Route path="/random-quote-generator">
+          <Quote />
+        </Route>
+        <Route path="/markdown-previewer">
+          <Markdown />
+        </Route>
+        <Route path="/">
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
-export default App;
