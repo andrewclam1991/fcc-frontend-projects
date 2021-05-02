1a5dc5950f243e8ad276e76d20cb391547ac0f2c
ANDREW LAM <23492362+andrewclam1991@users.noreply.github.com>
2021-05-02 11:22:43 -0700

    Adds working previewer

---
+++ src/features/markdown/editor/MarkdownEditor.tsx
@@ -0,0 +1,24 @@
+import { Form } from "react-bootstrap"
+import { useAppSelector, useAppDispatch } from "../../../app/hooks";
+import { setInput } from "../../markdown/markdownSlice";
+
+export const MarkdownEditor = () => {
+  const input = useAppSelector(state => state.markdown.input);
+  const dispatch = useAppDispatch();
+
+  return <>
+    <Form>
+      <Form.Group>
+        <Form.Label>Editor</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={5}
+          value={input}
+          onChange={event => {
+            dispatch(setInput(event.target.value))
+          }}
+        />
+      </Form.Group>
+    </Form>
+  </>
+}
